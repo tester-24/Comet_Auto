178ef5de55088b71d445ef531a0001dcf2d6da6d
Saloni <tester1.elitetechno@gmail.com>
2024-09-12 09:42:28 +0530

    Reports_Collection_Selling

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 const { allureCypress } = require("allure-cypress/reporter");
 
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
